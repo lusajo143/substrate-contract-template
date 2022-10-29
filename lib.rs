0fefa3d2f94cb6ec47bbc301fd7bff23b5a2611b
lusajo143 <techdealers143@gmail.com>
2022-10-29 03:14:40 +0300

    status code and message return types

--- lib.rs
+++ lib.rs
@@ -6,12 +6,20 @@ use ink_lang as ink;
 #[ink::contract]
 mod todo_contract {
 
+    use core::iter::Map;
+
     use ink_prelude::{
         string::{
             String,
             ToString as to_string,
         },
         vec::Vec
+    };
+
+    use scale::{Encode, Decode};
+
+    use ink_storage::traits::{
+        SpreadLayout, PackedLayout
     };
 
     mod users {
@@ -67,6 +75,8 @@ mod todo_contract {
     }
 
     /// Struct and its implementation for storing user tasks
+    #[derive(Debug, Clone, Encode, Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     struct Task {
         task_name: String,
         task_date: String,
@@ -76,6 +86,7 @@ mod todo_contract {
     /// Traits
     trait TaskManagement {
         fn new() -> Self;
+        fn on_error() -> Self;
     }
 
     impl TaskManagement for Task {
@@ -86,7 +97,48 @@ mod todo_contract {
                 is_done: false
             }
         }
+        fn on_error() -> Task {
+            Task {
+                task_name: String::from("null"),
+                task_date: String::from("null"),
+                is_done: false
+            }
+        }
     }
+
+
+    /// Responses
+    pub struct BaseResponse {
+        code: u32,
+        message: String
+    }
+    #[derive(Debug, Clone, Encode, Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct DataResponse<T> {
+        code: u32,
+        message: String,
+        data: T
+    }
+    enum StatusCodes {
+        CREATED(String),
+        UPDATED(String),
+        NULL(String),
+        INTERNAL_ERROR(String)
+    }
+    pub const OPERATION_STATUS: &str = "Operation successfully!";
+    pub const CREATED_STATUS: &str = "Created successfully!";
+    pub const UPDATED_STATUS: &str = "Updated successfully!";
+    pub const NULL_STATUS: &str = "Null Argument!";
+    pub const NOT_FOUND_STATUS: &str = "Not found";
+    pub const INTERNAL_ERROR_STATUS: &str = "Internal Constract Error!";
+    
+    pub const SUCCESS_CODE: u32 = 9000;
+    pub const NULL_CODE: u32 = 9001;
+    pub const NOT_FOUND_CODE: u32 = 9002;
+    pub const INTERNAL_ERROR_CODE: u32 = 9005;
+
+
+
 
     use ink_storage::Mapping;
     use ink_storage::traits::SpreadAllocate;
@@ -97,8 +149,8 @@ mod todo_contract {
     #[ink(storage)]
     #[derive(SpreadAllocate)]
     pub struct TodoContract {
-        user: Mapping<AccountId, users::User>
-        
+        user: Mapping<AccountId, users::User>,
+        tasks: Mapping<AccountId, Task>
     }
 
     impl TodoContract {
@@ -124,16 +176,21 @@ mod todo_contract {
         }
 
         #[ink(message)]
-        pub fn get_my_account(&self) -> users::User {
+        pub fn get_my_account(&self) -> DataResponse<users::User> {
             match self.user.get(Self::env().caller()) {
                 Some(data) => {
-                    return data
+                    DataResponse { code: SUCCESS_CODE, message: String::from(OPERATION_STATUS), data }
+                    
                 },
                 None => {
-                    return users::User::get_user_on_error()
+                    DataResponse { code: NOT_FOUND_CODE, message: String::from(NOT_FOUND_STATUS), data: users::User::get_user_on_error() } 
                 }
             }
+            
         }
+
+        // #[ink(message)]
+        // pub fn add_task(&self) -> 
 
     }
 
